ad7b923d98be7be2a74e123511652900d44a1bbb
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2023-12-05 19:12:42 +0300

    Added solution for Keyboard Row

---
+++ leet-code-solutions/keyboard-row.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        row = {}
+        ans = []
+        for c in "qwertyuiop":
+            row[c] = 1
+        for c in "asdfghjkl":
+            row[c] = 2
+        for c in "zxcvbnm":
+            row[c] = 3
+        
+        for word in words:
+            r = row[word[0].lower()]
+            one_row = True
+            for char in word:
+                if row[char.lower()] != r:
+                    one_row = False
+                    break
+            if one_row:
+                ans.append(word)
+        return ans
+            
+                
+        
