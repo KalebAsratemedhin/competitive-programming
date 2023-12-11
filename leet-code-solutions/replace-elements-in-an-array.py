03fdc1f7dc4f58e7869e89cb58bcd60bffcc5554
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2023-12-11 17:45:56 +0300

    Added solution for Replace Elements in an Array

---
+++ leet-code-solutions/replace-elements-in-an-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        dict = {}
+        for key, value in enumerate(nums):
+            dict[value] = key
+
+        for op in operations:
+            nums[dict[op[0]]] = op[1]
+            dict[op[1]] = dict[op[0]]
+        
+        return nums
