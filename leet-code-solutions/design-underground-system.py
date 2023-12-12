770b0d4119d0824bdec5d095fb50314ea2cde811
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2023-12-12 21:36:13 +0300

    Added solution for Design Underground System

---
+++ leet-code-solutions/design-underground-system.py
@@ -0,0 +1,31 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.check_in = {}
+        self.diff = {}
+        self.averageTime = {}
+        self.freq = {}
+        
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.check_in[id] = [stationName, t]            
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        self.diff[(self.check_in[id][0], stationName)] = self.diff.get((self.check_in[id][0], stationName), 0) + t - self.check_in[id][1]
+        self.freq[(self.check_in[id][0], stationName)] = self.freq.get((self.check_in[id][0], stationName), 0) + 1
+
+        self.averageTime[(self.check_in[id][0], stationName)] = self.diff[(self.check_in[id][0], stationName)] / self.freq[(self.check_in[id][0], stationName)]
+      
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        return self.averageTime[(startStation,endStation)]
+
+        
+
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
