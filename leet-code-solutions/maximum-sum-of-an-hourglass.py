72ced2b97924cf9e9e0ae549551b18b69a7e2fed
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2023-12-25 00:11:38 +0300

    Added solution for Maximum Sum of an Hourglass

---
+++ leet-code-solutions/maximum-sum-of-an-hourglass.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        maximal = 0
+        for i in range(m - 2):
+            for j in range(n - 2):
+                sum = 0
+                for k in range(3):
+                    sum += grid[i][j + k]
+                    sum += grid[i + 2][j + k]
+                sum += grid[i + 1][j + 1]
+                maximal = max(sum, maximal)
+        return maximal
+            
+
+
