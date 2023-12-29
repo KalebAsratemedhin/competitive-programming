07d8f3d1cb4f0b0a838dc41c724e86501c4d0b8c
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2023-12-29 18:01:05 +0300

    Added solution for Smallest Value of the Rearranged Number

---
+++ leet-code-solutions/smallest-value-of-the-rearranged-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestNumber(self, num: int) -> int:
+        s = [c for c in str(abs(num))]
+        s.sort()
+        zero_cutoff = 0
+        for c in s:
+            if c == '0':
+                zero_cutoff += 1
+            else:
+                break
+        if num >= 0:
+            ans = s[zero_cutoff: zero_cutoff + 1] + s[:zero_cutoff] + s[zero_cutoff + 1:]
+            ans = int("".join(ans))
+        else:
+            ans = s[::-1]
+            ans = -1 * int("".join(ans))
+        return ans
