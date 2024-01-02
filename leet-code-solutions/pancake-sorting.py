99ca723df7bc736de5ed0665db38bb902c701a55
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-01-02 18:26:43 +0300

    Added solution for Pancake Sorting

--- leet-code-solutions/pancake-sorting.py
+++ leet-code-solutions/pancake-sorting.py
@@ -2,26 +2,27 @@ class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         n = len(arr)
         flips = []
-        for j in range(n):
-            max_index = 0
-            for i in range(n - j):
-                if arr[i] > arr[max_index]:
-                    max_index = i
-            l = 0
-            r = max_index
-            if l != r:
+        for j in range(n, 1, -1):
+            max_index = arr.index(j)
+            if max_index == j - 1:
+                continue
+            
+            if max_index != 0:
+                r = max_index
+                l = 0
+                while l < r:
+                    arr[l], arr[r] = arr[r], arr[l]
+                    l += 1
+                    r -= 1
                 flips.append(max_index + 1)
-            while l < r:
-                arr[l], arr[r] = arr[r], arr[l]
-                l += 1
-                r -= 1
+            r = j - 1
             l = 0
-            r = n - 1 - j
-            flips.append(r + 1)
             while l < r:
                 arr[l], arr[r] = arr[r], arr[l]
                 l += 1
                 r -= 1
+            flips.append(j)
+           
         return flips
             
         
