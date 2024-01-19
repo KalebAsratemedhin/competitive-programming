b11a04fa1235075ce4d63f3dc254b6a9271015b2
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-01-19 21:50:24 +0300

    Added solution for Find the Middle Index in Array

---
+++ leet-code-solutions/find-the-middle-index-in-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum = 0
+        left = 0
+        for num in nums:
+            sum += num
+        for i in range(n):
+            sum -= nums[i]
+            if left == sum:
+                return i
+            left += nums[i]
+        return -1
