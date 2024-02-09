d3955adc5cf0efcb6994bc262e2789cd454ee584
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-02-09 10:30:34 +0300

    Add solution for Sum of Absolute Differences in a Sorted Array

--- leetcode/sum-of-absolute-differences-in-a-sorted-array.py
+++ leetcode/sum-of-absolute-differences-in-a-sorted-array.py
@@ -4,12 +4,28 @@ class Solution:
         sum = 0
         ref = nums[0]
         result = []
-        for i in range(1, n):
-            sum += nums[i] - ref
-        result.append(sum)
-        for i in range(1, n):
-            diff = nums[i] - ref
-            ref = nums[i]
-            sum += (-diff * (n - i) + i * diff)
-            result.append(sum)
+        suffix = [0] * n
+        prefix = [0] * n
+
+        sum = 0
+
+        for i in range(n):
+            sum += nums[i]
+            prefix[i] = sum
+        
+        sum = 0
+        for j in range(n - 1, -1, -1):
+            sum += nums[j]
+            suffix[j] = sum
+        
+        for i in range(n):
+            res = 0
+            if i > 0:
+                res += nums[i] * i - prefix[i - 1]
+            if i < n - 1:
+                res += suffix[i + 1] - (n - i - 1) * nums[i]
+            result.append(res)
+
+
+        
         return result
