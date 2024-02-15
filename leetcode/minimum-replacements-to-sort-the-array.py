21273805c3afbddf425136b31bbe58f3aed3e0a1
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-02-15 09:46:06 +0300

    Add solution for Minimum Replacements to Sort the Array

---
+++ leetcode/minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        operations = 0
+        base = nums[-1]
+        n = len(nums)
+        for i in range(n - 2, -1, -1):
+            if nums[i] % base == 0:
+                spaces = nums[i] // base
+            else:
+                spaces = nums[i] // base + 1
+            base = nums[i] // spaces
+            operations += spaces - 1
+        return operations
+
+
+
+
