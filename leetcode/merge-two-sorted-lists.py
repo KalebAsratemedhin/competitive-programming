4ab5412b1c9908a2043e4d3b6ee8fcf17939ba26
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-02-20 20:55:08 +0300

    Add solution for Merge Two Sorted Lists

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -5,32 +5,30 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        curr1 = list1
-        curr2 = list2
-        prev = ListNode()
-        head = prev
-        while curr1 or curr2:
-            if curr1 and curr2:
-                if curr1.val < curr2.val:
-                    prev.next = curr1
-                    prev = curr1
-                    curr1 = curr1.next
-                    
-                else:
-                    prev.next = curr2
-                    prev = curr2
-                    curr2 = curr2.next
-            
-                
-            elif curr1:
-                prev.next = curr1
-                prev = curr1
-                curr1 = curr1.next
-                
-            elif curr2:
-                prev.next = curr2
-                prev = curr2
-                curr2 = curr2.next
-            
+        def merge(prev, node1, node2):
+            if not node1 and not node2:
+                return 
+            if not node1:
+                prev.next = node2
+                prev = node2
+                merge(prev, node1, node2.next)
+                return
+            if not node2:
+                prev.next = node1
+                prev = node1
+                merge(prev, node1.next, node2)
+                return
+
+            if node1.val < node2.val:
+                prev.next = node1
+                prev = node1
+                merge(prev, node1.next, node2)
+            else:
+                prev.next = node2
+                prev = node2
+                merge(prev, node1, node2.next)
+            return
+        
+        head = ListNode()
+        merge(head, list1, list2)
         return head.next
-                
