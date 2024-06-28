6b5c13a55f47906bdd348c9af8ec11d35ae73880
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-06-28 23:00:38 +0300

    Created 'String Compression' by A2SV Hub

---
+++ 28-Jun-2024/String Compression 174287.py
@@ -0,0 +1,34 @@
+# Problem: String Compression - https://leetcode.com/problems/string-compression/
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        seeker, placeholder = 0, 0
+        curr = 0
+
+        while seeker < len(chars):
+            length = 0
+            while seeker < len(chars) and chars[seeker] == chars[curr]:
+                length += 1
+                seeker += 1
+            
+            
+            if length == 1:
+                chars[placeholder] = chars[curr]
+                placeholder += 1
+            else:
+                b = len(str(length)) - 1
+                m = 10 ** b
+                chars[placeholder] = chars[curr]
+                placeholder += 1
+
+                while m >= 1:
+                    chars[placeholder] = str(int(length // m))
+                    length %= m
+                    m /= 10
+                    placeholder += 1
+            curr = seeker
+        print(chars)
+        return placeholder
+                
+
+            
