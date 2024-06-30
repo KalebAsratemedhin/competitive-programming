250d9244f9c223a7cfd70f912433d4c8a358bdfb
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-06-30 19:59:00 +0300

    Created 'Most Profit Assigning Work' by A2SV Hub

---
+++ 30-Jun-2024/Most Profit Assigning Work 174917.py
@@ -0,0 +1,15 @@
+# Problem: Most Profit Assigning Work - https://leetcode.com/problems/most-profit-assigning-work/
+
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        arr = [(profit[i], difficulty[i]) for i in range(len(difficulty))]
+        arr.sort(reverse = True)
+        total_profit = 0
+         
+        for ability in worker:
+            for prof, diff in arr:
+                if ability >= diff:
+                    total_profit += prof
+                    break
+                    
+        return total_profit
