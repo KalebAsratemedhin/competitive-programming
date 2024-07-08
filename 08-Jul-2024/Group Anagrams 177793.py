2764846b85e7a8384ebe18ffb2528803e3f27cd1
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-08 18:05:11 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 08-Jul-2024/Group Anagrams 177793.py
@@ -0,0 +1,15 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+        for i in range(len(strs)):
+            key = str(sorted(strs[i]))
+            if key in res:
+                res[key].append(strs[i])
+            else:
+                res[key] = [strs[i]]
+        
+        return [values for values in res.values()]
+
+        
