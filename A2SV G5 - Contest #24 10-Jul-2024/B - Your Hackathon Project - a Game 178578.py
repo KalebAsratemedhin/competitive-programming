958e4bfd00e8b806944f65da77bfdd481c39dbaf
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-10 19:23:29 +0300

    Created 'B - Your Hackathon Project - a Game' by A2SV Hub

---
+++ A2SV G5 - Contest #24 10-Jul-2024/B - Your Hackathon Project - a Game 178578.py
@@ -0,0 +1,29 @@
+# Problem: B - Your Hackathon Project - a Game - https://codeforces.com/gym/534160/problem/B
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+prefix = [0 for i in range(n)]
+suffix = [0 for i in range(n)]
+
+
+
+for i in range(1, len(arr)):
+    if arr[i] < arr[i - 1]:
+        prefix[i] = prefix[i - 1] + (arr[i - 1] - arr[i])
+    
+    else:
+        prefix[i] = prefix[i - 1] 
+
+    if arr[n - 1 - i] < arr[n - i]:
+        suffix[n - 1 - i] = suffix[n - i] + (arr[n - i] - arr[n - 1 - i])
+    else:
+        suffix[n - 1 - i] = suffix[n - i]
+
+
+for _ in range(m):
+    s, t = map(int, input().split())
+    if s < t:
+        print(prefix[t - 1] - prefix[s - 1])
+    else:
+        print(suffix[t - 1] - suffix[s - 1])
+   
