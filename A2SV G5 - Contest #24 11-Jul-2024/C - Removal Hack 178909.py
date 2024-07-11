ecaee26d3f9b21b159ed3ae04f84c6034bb27c73
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-11 20:00:49 +0300

    Created 'C - Removal Hack' by A2SV Hub

---
+++ A2SV G5 - Contest #24 11-Jul-2024/C - Removal Hack 178909.py
@@ -0,0 +1,26 @@
+# Problem: C - Removal Hack - https://codeforces.com/gym/534160/problem/C
+
+n = int(input())
+children = [1 for i in range(n + 1)]
+disres = []
+ans = []
+for i in range(n):
+    p, c = map(int, input().split())
+    if p != -1:
+        children[p] *= c
+    if c:
+        disres.append(i + 1)
+
+
+disres.sort()
+
+for node in disres:
+    if children[node]:
+        ans.append(str(node))
+
+if not ans:
+    print(-1)
+else:
+    print(" ".join(ans))
+
+
