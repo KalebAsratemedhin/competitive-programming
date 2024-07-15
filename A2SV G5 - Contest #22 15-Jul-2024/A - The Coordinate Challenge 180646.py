012f3703c113b292d084155c3e552c058d414bfd
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-15 22:20:11 +0300

    Created 'A - The Coordinate Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #22 15-Jul-2024/A - The Coordinate Challenge 180646.py
@@ -0,0 +1,20 @@
+# Problem: A - The Coordinate Challenge - https://codeforces.com/gym/531455/problem/A
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n == 1:
+        print(2)
+    elif n % 3 == 0:
+        print(n // 3)
+    elif n % 3 == 1:
+        if n >= 4:
+            print ((n - 4) // 3 + 2  )
+        else:
+            print ((n - 2) // 3 + 1  ) 
+    else:
+        if n >= 2:
+            print ((n - 2) // 3 + 1  )
+        else:
+            print ((n - 2) // 3 + 1  )
