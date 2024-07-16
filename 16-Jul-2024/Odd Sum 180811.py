d7265085fdb0aa99d2dbbc9a338647e6789889f1
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-16 15:17:42 +0300

    Created 'Odd Sum' by A2SV Hub

---
+++ 16-Jul-2024/Odd Sum 180811.py
@@ -0,0 +1,42 @@
+# Problem: Odd Sum - https://codeforces.com/problemset/problem/797/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort(reverse=True)
+
+pos_odd = []
+neg_odd = []
+even_sum = 0
+
+
+for num in arr:
+    if num & 1:
+        if num > 0:
+            pos_odd.append(num)
+        elif num < 0:
+            neg_odd.append(num)
+
+    elif num > 0:
+        even_sum += num
+    
+
+if len(pos_odd) % 2:
+    print(even_sum + sum(pos_odd))
+elif len(pos_odd) > 0:
+    diff = pos_odd.pop()
+    ans = even_sum + sum(pos_odd)
+
+    if len(neg_odd):
+        evn = diff + neg_odd[0]
+        if evn > 0:
+            ans += evn
+    print(ans)
+
+elif neg_odd:
+    print(even_sum + neg_odd[0])
+
+
+
+
+
+
