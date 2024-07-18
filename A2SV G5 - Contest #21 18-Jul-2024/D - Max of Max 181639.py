65e9ae5b148f5ab3db63cf0e31d5445fbe97eaf9
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-18 22:23:03 +0300

    Created 'D - Max of Max' by A2SV Hub

---
+++ A2SV G5 - Contest #21 18-Jul-2024/D - Max of Max 181639.py
@@ -0,0 +1,46 @@
+# Problem: D - Max of Max - https://codeforces.com/gym/530187/problem/D
+
+def check(max_val):
+
+    for i in range(n):
+
+        cost = 0
+        curr = max_val
+        for j in range(i, n):
+            if a[j] < curr and j == n - 1:
+                cost = k + 1
+                break
+            
+            if a[j] >= curr:
+                break
+
+            cost += curr - a[j]
+            next = max(curr - 1, 0)
+            curr = next
+
+        if cost <= k:
+            return True
+
+    return False
+
+
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    low = max(a)
+    high = max(a) + min(k, n)
+
+    while low <= high:
+
+        mid = low + (high - low)//2
+
+        if check(mid):
+            low = mid + 1
+        
+        else:
+            high = mid - 1
+    
+    print(high)
