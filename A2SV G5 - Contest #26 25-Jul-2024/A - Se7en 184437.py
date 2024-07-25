a53bfacee9cde5b657b115010bc04f34ec9e9a62
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-25 11:24:40 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/A - Se7en 184437.py
@@ -0,0 +1,18 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n % 7 == 0:
+        print(n)
+    
+    else:
+        rem = n % 7
+        d = str(n)
+        if int(d[-1]) - rem >= 0:
+            print(n - rem)
+        else:
+            print(n + (7 - rem))
+
+
+
