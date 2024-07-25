0de045cddff39abe73b7109d50cd401e0a78a703
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-07-25 11:22:29 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/B - Mohammed's Magical Cleaning Machine 184435.py
@@ -0,0 +1,24 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+t = int(input())
+
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    ops = 0
+    pointer = 0
+
+    for i in range(n - 1):
+        
+        if not arr[i]:
+            if arr[pointer] > 0:
+                arr[i] += 1
+                ops += 1
+                arr[pointer] -= 1
+            if not arr[pointer]:
+                pointer += 1
+            
+    
+    print(ops + sum(arr[:-1]))
+
