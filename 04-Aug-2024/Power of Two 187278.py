f62c8f4638994c3422a7ca0043eda1aee41db132
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-08-04 19:51:43 +0300

    Created 'Power of Two' by A2SV Hub

---
+++ 04-Aug-2024/Power of Two 187278.py
@@ -0,0 +1,7 @@
+# Problem: Power of Two - https://leetcode.com/problems/power-of-two/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return n.bit_count() == 1
