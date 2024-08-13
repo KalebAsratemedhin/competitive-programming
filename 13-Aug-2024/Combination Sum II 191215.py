76370890615a2cc8e0f5ab0c5d1e205bcae24ea6
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-08-13 20:26:18 +0300

    Created 'Combination Sum II' by A2SV Hub

---
+++ 13-Aug-2024/Combination Sum II 191215.py
@@ -0,0 +1,25 @@
+# Problem: Combination Sum II - https://leetcode.com/problems/combination-sum-ii/
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        arr = []
+        candidates.sort()
+
+        def helper(start):
+            if sum(arr) == target:
+                ans.append(arr.copy())
+
+            for i in range(start, len(candidates)):
+                if sum(arr) > target:
+                    return
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                arr.append(candidates[i])
+                helper(i + 1)
+                arr.pop()
+                
+        helper(0)
+
+        return ans
