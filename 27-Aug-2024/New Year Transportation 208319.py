c761482575670306857eb5e456a39b60059c9e47
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-08-27 20:05:27 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 27-Aug-2024/New Year Transportation 208319.py
@@ -0,0 +1,18 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, t = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+pos = 1
+
+while pos <= n:
+    if pos == t:
+        print("YES")
+        break
+    elif pos > t:
+        print("NO")
+        break
+
+    pos += arr[pos - 1]
+
