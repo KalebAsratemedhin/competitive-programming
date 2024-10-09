27a8485d2a9f5fc45e08873be732a00e0880dcbc
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2024-10-09 14:38:52 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 09-Oct-2024/Ehab Is an Odd Person 218987.py
@@ -0,0 +1,20 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+even = odd = 0
+for num in arr:
+    if num & 1:
+        odd += 1
+    else:
+        even += 1
+    
+
+if even and odd:
+    arr.sort()
+
+
+for a in arr:
+    print(a, end=" ")
+
