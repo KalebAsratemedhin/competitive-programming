8aa967fc149521bd64b4820a4578a993a1d4337b
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-04-21 16:16:02 +0300

    Add solution to Minimum Index of a Valid Split at Mon Apr 21 2025 16:16:01 GMT+0300 (East Africa Time)

---
+++ leetcode/Mon Apr 21 2025 16:16:01 GMT+0300 (East Africa Time)_Minimum_Index_of_a_Valid_Split.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        dict = {}
+        dominant = nums[0]
+        for i in range(n):
+            if nums[i] in dict:
+                dict[nums[i]] += 1
+            else:
+                dict[nums[i]] = 1
+            if dict[nums[i]] > dict[dominant]:
+                dominant = nums[i]
+        size = 0
+        count = 0
+        for i in range(n - 1):
+            if nums[i] == dominant:
+                size += 1
+                count += 1
+                if count * 2 > size and (dict[dominant] - count) * 2 > (n - size):
+                    return i
+            else:
+                size += 1
+        return -1
