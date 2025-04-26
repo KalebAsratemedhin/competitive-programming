f56ca76448a024a7df484a183bcf7edee21db145
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-04-26 13:22:48 +0300

    2527-COUNT-SUBARRAYS-WITH-FIXED-BOUNDS Stats: Time: 106 ms (93.59%), Space: 28.8 MB (85.51%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.py
@@ -1,22 +1,22 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        last_bad = -1
-        min_pointer = -1
-        max_pointer = -1
-        count = 0
 
-        for i in range(len(nums)):
-            if nums[i] < minK or nums[i] > maxK:
-                min_pointer = -1
-                max_pointer = -1
-                last_bad = i
-            if nums[i] == minK:
-                min_pointer = i
-            if nums[i] == maxK:
-                max_pointer = i
-            
-            if min_pointer != -1 and max_pointer != -1:
-                count += min(min_pointer, max_pointer) - last_bad
+        near_min = near_max = -1
+        start = count = 0
+        
+        for index, num in enumerate(nums):
+            if num == minK:  
+                near_min = index
+            if num == maxK: 
+                near_max = index
+            if not (minK <= num <= maxK):
+                start = index + 1
+                near_min = near_max = -1
+
+            if near_max != -1 and near_min != -1:
+                count += min(near_max, near_min) - start + 1
+
 
         return count
-                
+
+
