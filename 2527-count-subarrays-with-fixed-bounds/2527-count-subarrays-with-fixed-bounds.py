c69dde2f9e9de99e49c41cf6e74f1d1409bb018d
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-04-26 13:14:53 +0300

    2527-COUNT-SUBARRAYS-WITH-FIXED-BOUNDS Stats: Time: 111 ms (88.84%), Space: 28.8 MB (46.56%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.py
@@ -1,36 +1,22 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        left = start = count = 0
-        window = defaultdict(int)
+        last_bad = -1
+        min_pointer = -1
+        max_pointer = -1
+        count = 0
 
-        # 1,3,5,1,2,5,5
-        #  1 1 1 1
-
-        if minK >  maxK:
-            return 0
-        near_min = near_max = near_pair = -1
-        
-        for index, num in enumerate(nums):
-            if num == minK:
-                if minK == maxK:
-                    near_max = index
-                if near_max != -1:
-                    count += near_max - start + 1
-                    near_pair = near_max   
-                near_min = index
-            elif num == maxK:
-                if near_min != -1:
-                    count += near_min - start + 1
-                    near_pair = near_min   
-
-                near_max = index
-            elif minK <= num <= maxK:
-                if near_pair != -1:
-                    count += near_pair - start + 1
-            else:
-                start = index + 1
-                near_min = near_max = near_pair = -1
+        for i in range(len(nums)):
+            if nums[i] < minK or nums[i] > maxK:
+                min_pointer = -1
+                max_pointer = -1
+                last_bad = i
+            if nums[i] == minK:
+                min_pointer = i
+            if nums[i] == maxK:
+                max_pointer = i
+            
+            if min_pointer != -1 and max_pointer != -1:
+                count += min(min_pointer, max_pointer) - last_bad
 
         return count
-
-
+                
