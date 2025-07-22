0a1d4617cb4056e3fe44c1f32521073df40dd1f8
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:24:26 +0300

    Create 2816-LexicographicallySmallestPalindrome

---
+++ 2816-LexicographicallySmallestPalindrome/2816-LexicographicallySmallestPalindrome.py
@@ -0,0 +1,16 @@
+# Last updated: 7/22/2025, 3:24:26 PM
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        left = 0
+        right = len(s) - 1
+        ans = list(s)
+
+        while left < right:
+            if ord(ans[left]) < ord(ans[right]):
+                ans[right] = ans[left]
+            elif ord(ans[left]) > ord(ans[right]):
+                ans[left] = ans[right]
+            left += 1
+            right -= 1
+            
+        return "".join(ans)
