d376222a84c2f0b4bebd6dd93ced4b60a37099ec
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:25:17 +0300

    Create 2581-DividePlayersIntoTeamsOfEqualSkill

---
+++ 2581-DividePlayersIntoTeamsOfEqualSkill/2581-DividePlayersIntoTeamsOfEqualSkill.py
@@ -0,0 +1,20 @@
+# Last updated: 7/22/2025, 3:25:16 PM
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        n = len(skill)
+        skill.sort()
+        left  = 0
+        right = n - 1
+        
+        ans = skill[left] + skill[right]
+        chemistries = 0
+
+        while left < right:
+            if skill[left] + skill[right] == ans:
+                chemistries += skill[left] * skill[right]
+                left += 1
+                right -= 1
+            else:
+                return -1
+        return chemistries
+        
