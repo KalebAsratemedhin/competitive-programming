cce9c04d455336b35de4dce4a3bad8a97b90e5b6
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:24:16 +0300

    Create 2891-MaximumBeautyOfAnArrayAfterApplyingOperation

---
+++ 2891-MaximumBeautyOfAnArrayAfterApplyingOperation/2891-MaximumBeautyOfAnArrayAfterApplyingOperation.py
@@ -0,0 +1,18 @@
+# Last updated: 7/22/2025, 3:24:16 PM
+
+class Solution:
+    def maximumBeauty(self, nums: list[int], k: int) -> int:
+        events = []
+        for num in nums:
+            events.append((num - k, 1))  
+            events.append((num + k + 1, -1)) 
+
+        events.sort()
+
+        max_beauty = 0
+        current_count = 0
+        for value, effect in events:
+            current_count += effect
+            max_beauty = max(max_beauty, current_count)
+
+        return max_beauty
