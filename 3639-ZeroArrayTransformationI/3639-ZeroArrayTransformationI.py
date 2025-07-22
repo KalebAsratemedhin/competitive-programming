bfd8959992ec6e06a9256ac0fb4bed81362d77ae
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:22:56 +0300

    Create 3639-ZeroArrayTransformationI

---
+++ 3639-ZeroArrayTransformationI/3639-ZeroArrayTransformationI.py
@@ -0,0 +1,17 @@
+# Last updated: 7/22/2025, 3:22:55 PM
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        prefix = [0 for i in range(len(nums))]
+
+        for l, r in queries:
+            prefix[l] += 1
+            if r + 1 < len(nums):
+                prefix[r + 1] -= 1  
+        
+        for i in range(len(nums)):
+            if i > 0:
+                prefix[i] += prefix[i - 1]
+            if prefix[i] < nums[i]:
+                return False
+                
+        return True
