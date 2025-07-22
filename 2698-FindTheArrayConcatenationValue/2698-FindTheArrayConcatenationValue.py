2c450884a669ab9516ef2fdb61081d4fba79c149
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:24:51 +0300

    Create 2698-FindTheArrayConcatenationValue

---
+++ 2698-FindTheArrayConcatenationValue/2698-FindTheArrayConcatenationValue.py
@@ -0,0 +1,29 @@
+# Last updated: 7/22/2025, 3:24:51 PM
+class Solution:
+    def findTheArrayConcVal(self, nums: List[int]) -> int:
+        concat_value = 0
+        first = 0
+        last = len(nums) - 1
+
+        while first < last:
+            exp = len(str(nums[last]))
+            concat_value += nums[first] * 10 ** exp
+            concat_value += nums[last]
+            first += 1
+            last -= 1
+        if first == last:
+            concat_value += nums[first]
+        return concat_value
+
+
+
+
+
+
+
+
+
+
+
+
+
