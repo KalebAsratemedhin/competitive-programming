f8ef622c557428043284e6e959222211d6477418
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:23:30 +0300

    Create 3360-MinimumDeletionsToMakeStringKSpecial

---
+++ 3360-MinimumDeletionsToMakeStringKSpecial/3360-MinimumDeletionsToMakeStringKSpecial.py
@@ -0,0 +1,16 @@
+# Last updated: 7/22/2025, 3:23:29 PM
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        cnt = defaultdict(int)
+        for c in word:
+            cnt[c] += 1
+        res = len(word)
+        for a in cnt.values():
+            deleted = 0
+            for b in cnt.values():
+                if a > b:
+                    deleted += b
+                elif b > a + k:
+                    deleted += b - (a + k)
+            res = min(res, deleted)
+        return res
