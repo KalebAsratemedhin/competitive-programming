8a259aecc0b27316529453ddb6dd20c2e4288e10
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:23:13 +0300

    Create 3485-MaximizeScoreOfNumbersInRanges

---
+++ 3485-MaximizeScoreOfNumbersInRanges/3485-MaximizeScoreOfNumbersInRanges.py
@@ -0,0 +1,33 @@
+# Last updated: 7/22/2025, 3:23:13 PM
+class Solution:
+    def maxPossibleScore(self, start: List[int], d: int) -> int:
+
+        start.sort()
+        low = 0
+        high = (start[-1] + d - start[0]) // (len(start) - 1)
+
+        def check(sep):
+            arr = [start[0]]
+            for i in range(1, len(start)):
+                if arr[i - 1] + sep > start[i] + d:
+                    return False
+                if start[i] <= arr[i - 1] + sep <= start[i] + d:
+                    arr.append(arr[i - 1] + sep)
+                else:
+                    arr.append(start[i])
+                    
+            return True
+
+        
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if check(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return high
+
+
