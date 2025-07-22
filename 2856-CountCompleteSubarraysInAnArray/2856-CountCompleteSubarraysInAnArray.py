cd9ca6378b135a9481574add8ed87f571af0d8d0
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:24:21 +0300

    Create 2856-CountCompleteSubarraysInAnArray

---
+++ 2856-CountCompleteSubarraysInAnArray/2856-CountCompleteSubarraysInAnArray.py
@@ -0,0 +1,24 @@
+# Last updated: 7/22/2025, 3:24:21 PM
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        window = defaultdict(int)
+        distinct = len(set(nums))
+
+        left = 0
+        count = 0
+        n = len(nums)
+
+        for right in range(n):
+            window[nums[right]] += 1
+            while len(window) >= distinct :
+                count += n - right
+                window[nums[left]] -= 1
+                if window[nums[left]] == 0:
+                    del window[nums[left]]
+                left += 1
+        
+        return count
+
+
+
+
