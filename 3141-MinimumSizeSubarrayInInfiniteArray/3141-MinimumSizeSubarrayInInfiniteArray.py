85cd0feac6ad9eb3ae4940e7080f8c4e2451cb68
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:24:07 +0300

    Create 3141-MinimumSizeSubarrayInInfiniteArray

---
+++ 3141-MinimumSizeSubarrayInInfiniteArray/3141-MinimumSizeSubarrayInInfiniteArray.py
@@ -0,0 +1,33 @@
+# Last updated: 7/22/2025, 3:24:07 PM
+class Solution:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
+        
+        sum_ = sum(nums)
+
+        def findMinSubarray(total, nums):
+            temp = nums + nums
+            prefix = {0: -1}
+            length = float("inf")
+            num = 0
+            for i in range(len(temp)):
+                num += temp[i]
+                if num - total in prefix:
+                    length = min(length, i - prefix[num - total])
+                prefix[num] = i
+            return length if length != float("inf") else -1
+        if sum_ == target:
+            return len(nums)
+        elif target < sum_:
+            return findMinSubarray(target, nums) 
+        else:
+            ans = target // sum_
+            target %= sum_
+            if not target:
+                return ans * len(nums)
+            temp = findMinSubarray(target, nums)
+            
+            return temp + ans * len(nums) if temp != -1 else -1
+    
+
+
+        
