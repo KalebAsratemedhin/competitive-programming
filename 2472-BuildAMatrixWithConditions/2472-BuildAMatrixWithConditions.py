13ddeae93d43acde50cf8261a49f5005433f026a
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:25:49 +0300

    Create 2472-BuildAMatrixWithConditions

---
+++ 2472-BuildAMatrixWithConditions/2472-BuildAMatrixWithConditions.py
@@ -0,0 +1,41 @@
+# Last updated: 7/22/2025, 3:25:49 PM
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        def topSort(adjList):
+            graph = [[] for i in range(k)]
+            indegrees = [0 for i in range(k)]
+
+            for u, v in adjList:
+                graph[u - 1].append(v - 1)
+                indegrees[v - 1] += 1
+            
+            queue = deque()
+            for i in range(k):
+                if indegrees[i] == 0:
+                    queue.append(i)
+
+            order = []
+            while queue:
+                node = queue.popleft()
+                order.append(node + 1)
+                for adj in graph[node]:
+                    indegrees[adj] -= 1
+                    if indegrees[adj] == 0:
+                        queue.append(adj)
+            
+            return order
+
+        rowOrder = topSort(rowConditions)
+        colOrder = topSort(colConditions)
+
+        if len(rowOrder) < k or len(colOrder) < k:
+            return []
+
+        matrix = [[0 for i in range(k)] for j in range(k)]
+
+        for i in range(k):
+            for j in range(k):
+                if rowOrder[i] == colOrder[j]:
+                    matrix[i][j] = rowOrder[i]
+        return matrix
+
