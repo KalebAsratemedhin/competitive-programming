a3802050d09ab0dae1e4d50902aa5e6309b8da65
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:27:22 +0300

    Create 2134-MaximizeTheConfusionOfAnExam

---
+++ 2134-MaximizeTheConfusionOfAnExam/2134-MaximizeTheConfusionOfAnExam.py
@@ -0,0 +1,20 @@
+# Last updated: 7/22/2025, 3:27:22 PM
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        n = len(answerKey)
+        maxima = 0  
+        i = 0  
+        counts = {'T': 0, 'F': 0}  
+
+        for j in range(n):
+            counts[answerKey[j]] += 1
+
+            while min(counts['T'], counts['F']) > k:
+                counts[answerKey[i]] -= 1
+                i += 1
+
+            maxima = max(maxima, j - i + 1)
+
+        return maxima
+                
+            
