59623b60fe62fe3017bea6d87eeb95e81ef50ae8
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:24:18 +0300

    Create 2888-MinimumIndexOfAValidSplit

---
+++ 2888-MinimumIndexOfAValidSplit/2888-MinimumIndexOfAValidSplit.py
@@ -0,0 +1,15 @@
+# Last updated: 7/22/2025, 3:24:17 PM
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        total = Counter(nums)
+        running = {}
+        n = len(nums)
+
+        for i, num in enumerate(nums):
+            total[num] -= 1
+            running[num] = running.get(num, 0) + 1
+
+            if running[num] > (i + 1) // 2 and total[num] > (n - i - 1) // 2:
+                return i
+        return -1
+
