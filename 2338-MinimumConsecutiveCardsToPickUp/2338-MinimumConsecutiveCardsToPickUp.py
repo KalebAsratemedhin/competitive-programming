d5edf6605ae54af168013241edd86911d8c98c8e
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:26:26 +0300

    Create 2338-MinimumConsecutiveCardsToPickUp

---
+++ 2338-MinimumConsecutiveCardsToPickUp/2338-MinimumConsecutiveCardsToPickUp.py
@@ -0,0 +1,16 @@
+# Last updated: 7/22/2025, 3:26:26 PM
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        seen = set()
+        n = len(cards)
+        left = 0
+        subarray_length = n + 1
+        for right in range(n):
+            while cards[right] in seen:
+                subarray_length = min(subarray_length, right - left + 1)
+                seen.discard(cards[left])
+                left += 1
+            seen.add(cards[right])
+        if subarray_length == n + 1:
+            return -1
+        return subarray_length
