95ad487a066a7509e50ab6cb8ebeb15621c2217c
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:26:57 +0300

    Create 2215-Finding3DigitEvenNumbers

---
+++ 2215-Finding3DigitEvenNumbers/2215-Finding3DigitEvenNumbers.py
@@ -0,0 +1,20 @@
+# Last updated: 7/22/2025, 3:26:57 PM
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        ans = []
+        cnt_ = Counter(digits)
+
+        for i in range(1, 10):
+            for j in range(10):
+                for k in [0, 2, 4, 6, 8]:
+                    lst = [str(i), str(j), str(k)]
+                    cnt = Counter(lst)
+                    num = int("".join(lst))
+                    valid = True
+                    for key, val in cnt.items():
+                        if cnt[key] > cnt_[int(key)]:
+                            valid = False
+                    if valid:
+                        ans.append(num)
+        return ans
+
