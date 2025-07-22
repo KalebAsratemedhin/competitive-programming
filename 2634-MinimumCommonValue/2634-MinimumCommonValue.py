0445f45ebc452311208d802903bc12e6974aa606
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:25:02 +0300

    Create 2634-MinimumCommonValue

---
+++ 2634-MinimumCommonValue/2634-MinimumCommonValue.py
@@ -0,0 +1,15 @@
+# Last updated: 7/22/2025, 3:25:02 PM
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        first = 0
+        second = 0
+        while first < len(nums1) and second < len(nums2):
+            if nums1[first] < nums2[second]:
+                first += 1
+            elif nums1[first] > nums2[second]:
+                second += 1
+            else:
+                return nums1[first]
+        return -1
+
+
