a3a6464c6a16c603d1d88b5902777d4cf9d231b3
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:27:39 +0300

    Create 2061-PaintingAGridWithThreeDifferentColors

---
+++ 2061-PaintingAGridWithThreeDifferentColors/2061-PaintingAGridWithThreeDifferentColors.py
@@ -0,0 +1,33 @@
+# Last updated: 7/22/2025, 3:27:39 PM
+class Solution:
+    def colorTheGrid(self, m: int, n: int) -> int:
+        mod = 10**9 + 7
+        valid = dict()
+
+        for mask in range(3**m):
+            color = list()
+            mm = mask
+            for i in range(m):
+                color.append(mm % 3)
+                mm //= 3
+            if any(color[i] == color[i + 1] for i in range(m - 1)):
+                continue
+            valid[mask] = color
+
+        adjacent = defaultdict(list)
+        for mask1, color1 in valid.items():
+            for mask2, color2 in valid.items():
+                if not any(x == y for x, y in zip(color1, color2)):
+                    adjacent[mask1].append(mask2)
+
+        f = [int(mask in valid) for mask in range(3**m)]
+        for i in range(1, n):
+            g = [0] * (3**m)
+            for mask2 in valid.keys():
+                for mask1 in adjacent[mask2]:
+                    g[mask2] += f[mask1]
+                    if g[mask2] >= mod:
+                        g[mask2] -= mod
+            f = g
+
+        return sum(f) % mod
