73e1c80ecc50a61544ab68c3dd8cafee5e256546
Kaleb <133399488+KalebAsratemedhin@users.noreply.github.com>
2025-07-22 15:27:04 +0300

    Create 2187-VowelsOfAllSubstrings

---
+++ 2187-VowelsOfAllSubstrings/2187-VowelsOfAllSubstrings.py
@@ -0,0 +1,27 @@
+# Last updated: 7/22/2025, 3:27:04 PM
+class Solution:
+    def countVowels(self, word: str) -> int:
+        left = right = total = 0
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        n = len(word)
+        base = 1
+
+        for i in range(n):
+            if word[i] in vowels:
+                break
+            left += 1
+            base += 1
+
+        if left < n:
+            total += base
+
+        for right in range(left + 1, n):
+
+            if word[right] not in vowels:
+                total += base
+            else:
+                total += base + (right + 1)
+                base += (right + 1)
+                left = right
+               
+        return total
